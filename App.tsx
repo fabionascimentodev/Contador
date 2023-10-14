1893f954f8a710bcad4751d16567d11ced92f589
fabionascimentodev <fabio.fbn1@gmail.com>
2023-10-14 00:28:01 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Contador from './components/counter/index'
+
+
+
+export default function App() {
+  return (
+  <>
+    <View style={styles.container}>
+      <Contador></Contador>
+    </View>
+ </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#B0E0E6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
